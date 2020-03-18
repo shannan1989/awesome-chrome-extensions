ad9a73b6bb9727583541345582c5101fae1e5c7b
山南 <zhangpeichaook@163.com>
2020-03-18 18:53:48 +0800

    #监听所有请求的响应头

--- src/background.js
+++ src/background.js
@@ -1,1 +1,8 @@
 console.log('Message from Mr Shannan!');
+
+// 监听所有请求的响应头
+chrome.webRequest.onResponseStarted.addListener(function (detail) {
+    console.log(detail);
+}, {
+    urls: ["<all_urls>"]
+}, ["responseHeaders"]);

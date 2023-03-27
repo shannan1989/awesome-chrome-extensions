cdc4f0b1758eac7756005f7d743637fb7c3ef421
山南 <zhangpeichaook@163.com>
2023-03-27 21:59:50 +0800

    #delete

--- src/target/fotor/main.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-    let img = document.createElement('img');
-    img.src = $('#theCanvasHolder>canvas')[0].toDataURL('image/png');
-    img.style = 'position:fixed;z-index:1000;bottom:0;right:0;width:160px;';
-    document.body.appendChild(img);
-}, 5000);
